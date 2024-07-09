a8470731f90174e7869684937c356ded924f3ce2
Dominick <drt1415@gmail.com>
2024-07-08 22:11:40 -0600

    feat: se agrega metodos get, set y str

--- cod/python/Voting_Classifier.py
+++ cod/python/Voting_Classifier.py
@@ -5,37 +5,66 @@ Modulo Voting_Classifier
 """
 
 from sklearn.ensemble import VotingClassifier
-import numpy as np
 from Modelo_XGB import ModeloXGBoost
 from Modelo_Logistico import ModeloLogistico
+from Modelo_Random_Forest import ModeloRandomForest
+from Modelo_Lightgbm import ModeloLightGBM
+from Modelo_SVC import ModeloSVC
 from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn.metrics import roc_auc_score
+import numpy as np
 
 class VotingClassifierModel:
-    def __init__(self, data, best_params_xgb=None, best_params_logistic=None):
+    def __init__(self, data, preproceso):
         self.data = data
-        self.X = self.data.drop(columns=['isFraud'])
-        self.Y = self.data['isFraud']
-        self.best_params_xgb = best_params_xgb
-        self.best_params_logistic = best_params_logistic
+        self.preproceso = preproceso
+        self._X = self.data.drop(columns=['isFraud'])
+        self._Y = self.data['isFraud']
         self.pipeline = None
 
-    def crear_modelos(self):
-        modelo_xgb = ModeloXGBoost(self.data, self.best_params_xgb)
-        modelo_logistico = ModeloLogistico(self.data, self.best_params_logistic)
-        modelo_xgb.crear_pipeline(self.best_params_xgb)
-        modelo_logistico.crear_pipeline(self.best_params_logistic)
-        return modelo_xgb.pipeline, modelo_logistico.pipeline
+    @property
+    def X(self):
+        return self._X
+
+    @X.setter
+    def X(self, new_data):
+        self._X = new_data
+
+    @property
+    def Y(self):
+        return self._Y
+
+    @Y.setter
+    def Y(self, new_data):
+        self._Y = new_data
+        
+    def __str__(self):
+        return "VotingClassifierModel con múltiples estimators"
+
+    def crear_modelos_base(self):
+        modelo_xgb = ModeloXGBoost(self.data, self.preproceso)
+        modelo_logistico = ModeloLogistico(self.data, self.preproceso)
+        modelo_rf = ModeloRandomForest(self.data, self.preproceso)
+        modelo_lgb = ModeloLightGBM(self.data, self.preproceso)
+        modelo_svc = ModeloSVC(self.data, self.preproceso)
+        
+        modelo_xgb.crear_pipeline()
+        modelo_logistico.crear_pipeline()
+        modelo_rf.crear_pipeline()
+        modelo_lgb.crear_pipeline()
+        modelo_svc.crear_pipeline()
+        
+        return [
+            ('xgb', modelo_xgb.pipeline),
+            ('logistic', modelo_logistico.pipeline),
+            ('rf', modelo_rf.pipeline),
+            ('lgb', modelo_lgb.pipeline),
+            ('svc', modelo_svc.pipeline)
+        ]
 
     def crear_voting_classifier(self):
-        pipeline_xgb, pipeline_logistic = self.crear_modelos()
-        self.pipeline = VotingClassifier(
-            estimators=[
-                ('xgb', pipeline_xgb),
-                ('logistic', pipeline_logistic)
-            ],
-            voting='soft'
-        )
+        base_estimators = self.crear_modelos_base()
+        self.pipeline = VotingClassifier(estimators=base_estimators, voting='soft', n_jobs=4)
         return self.pipeline
 
     def entrenar_voting_classifier(self):
@@ -47,10 +76,11 @@ class VotingClassifierModel:
         print("ROC AUC del conjunto de prueba:", roc_auc_test)
         return roc_auc_test
 
-    def validacion_cruzada(self, num_estratos=5):
+    def validacion_cruzada(self, num_estratos=10):
         self.crear_voting_classifier()
         estratos_kfold = StratifiedKFold(n_splits=num_estratos, shuffle=True, random_state=7)
-        roc_auc_vector = cross_val_score(self.pipeline, self.X, self.Y, cv=estratos_kfold, scoring='roc_auc', n_jobs=-1)
+        roc_auc_vector = cross_val_score(self.pipeline, self.X, self.Y, cv=estratos_kfold, scoring='roc_auc', n_jobs=4)
         roc_auc_promedio = np.mean(roc_auc_vector)
         print("ROC AUC promedio utilizando validación cruzada estratificada:", roc_auc_promedio)
         return roc_auc_promedio
+
